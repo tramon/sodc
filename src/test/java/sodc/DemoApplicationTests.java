ce40ae0d8e61c2235d0934e7f4a98680918b763b
tramon <andriitramon@gmail.com>
2025-03-04 12:25:49 +0200

    first commit

---
+++ src/test/java/sodc/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sodc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
